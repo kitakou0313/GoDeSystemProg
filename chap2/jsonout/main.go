16805d5c51200620aa07a6fdff2f849a0784acd8
kitakou0313 <kitakou0313@gmail.com>
2021-03-07 14:23:06 +0000

    Add sample of json

---
+++ chap2/jsonout/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func main() {
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.SetIndent("", " ")
+	encoder.Encode(map[string]string{
+		"sample": "hogehoge",
+	})
+}
