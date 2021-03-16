3df02a46bfa090eb745b50f17faa5f85a6e9c86c
kitakou0313 <kitakou0313@gmail.com>
2021-03-16 14:57:07 +0000

    Enable keep-alive

--- chap6/server.go
+++ chap6/server.go
@@ -3,11 +3,13 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net"
 	"net/http"
 	"net/http/httputil"
 	"strings"
+	"time"
 )
 
 func main() {
@@ -26,29 +28,35 @@ func main() {
 
 		go func() {
 			fmt.Printf("Accept %v\n", conn.RemoteAddr())
-
-			request, err := http.ReadRequest(bufio.NewReader(conn))
-
-			if err != nil {
-				panic(err)
+			for {
+				conn.SetReadDeadline(time.Now().Add(5 * time.Second))
+				request, err := http.ReadRequest(bufio.NewReader(conn))
+				if err != nil {
+					neterr, ok := err.(net.Error) // ダウンキャスト
+					if ok && neterr.Timeout() {
+						fmt.Println("Timeout")
+						break
+					} else if err == io.EOF {
+						break
+					}
+					panic(err)
+				}
+				dump, err := httputil.DumpRequest(request, true)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Println(string(dump))
+				content := "Hello World\n"
+				response := http.Response{
+					StatusCode:    200,
+					ProtoMajor:    1,
+					ProtoMinor:    1,
+					ContentLength: int64(len(content)),
+					Body: ioutil.NopCloser(
+						strings.NewReader(content)),
+				}
+				response.Write(conn)
 			}
-
-			dump, err := httputil.DumpRequest(request, true)
-			if err != nil {
-				panic(err)
-			}
-
-			fmt.Println(string(dump))
-
-			response := http.Response{
-				StatusCode: 200,
-				ProtoMajor: 1,
-				ProtoMinor: 1,
-				Body: ioutil.NopCloser(
-					strings.NewReader("Hello World\n"),
-				),
-			}
-			response.Write(conn)
 			conn.Close()
 		}()
 	}
