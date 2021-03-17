979463c1520c92f51a01d9428905a48ff25b5bca
kitakou0313 <kitakou0313@gmail.com>
2021-03-17 13:33:23 +0000

    Add client keep-alive

--- chap6/client.go
+++ chap6/client.go
@@ -6,30 +6,61 @@ import (
 	"net"
 	"net/http"
 	"net/http/httputil"
+	"strings"
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "localhost:8888")
-	if err != nil {
-		panic(err)
+	sendMsg := []string{
+		"ASCII",
+		"PROGRAMMING",
+		"PLUS",
 	}
 
-	request, err := http.NewRequest("GET", "http://localhost:8888", nil)
-	if err != nil {
-		panic(err)
+	current := 0
+
+	var conn net.Conn = nil
+
+	for {
+		var err error
+
+		if conn == nil {
+			conn, err = net.Dial("tcp", "localhost:8888")
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Access: %d\n", current)
+		}
+
+		req, err := http.NewRequest("POST", "http://localhost:8888", strings.NewReader(sendMsg[current]))
+		if err != nil {
+			panic(err)
+		}
+
+		err = req.Write(conn)
+		if err != nil {
+			panic(err)
+		}
+
+		response, err := http.ReadResponse(bufio.NewReader(conn), req)
+
+		if err != nil {
+			fmt.Println("Retry")
+			conn = nil
+			continue
+		}
+
+		dump, err := httputil.DumpResponse(response, true)
+
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(string(dump))
+
+		current++
+		if current == len(sendMsg) {
+			break
+		}
+
 	}
-
-	request.Write(conn)
-	response, err := http.ReadResponse(bufio.NewReader(conn), request)
-	if err != nil {
-		panic(err)
-	}
-
-	dump, err := httputil.DumpResponse(response, true)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(dump))
-
 }
