b8027e5f3627f003608437f04b1052cd0c8cb4ca
kitakou0313 <kitakou0313@gmail.com>
2021-03-22 13:58:29 +0000

    Add multi cast server and Client

---
+++ chap8/multiClient/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	fmt.Println("Listen ticl server at 224.0.0.1:9999")
+	addr, err := net.ResolveUDPAddr("udp", "224.0.0.1:9999")
+	if err != nil {
+		panic(err)
+	}
+
+	listener, err := net.ListenMulticastUDP("udp", nil, addr)
+	if err != nil {
+		panic(err)
+	}
+	defer listener.Close()
+
+	buffer := make([]byte, 1500)
+	for {
+		length, remoteAddr, err := listener.ReadFromUDP(buffer)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Printf("Server %v\n", remoteAddr)
+		fmt.Printf("Now %s\n", string(buffer[:length]))
+	}
+}
