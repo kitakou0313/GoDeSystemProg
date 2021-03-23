5318cff7df7a7c65efbca516a2c5bcc7c9c658cf
kitakou0313 <kitakou0313@gmail.com>
2021-03-23 14:28:08 +0000

    Add unix domain socker server sample

---
+++ chap9/httpWithUnixSocket/client.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"net/http"
+	"net/http/httputil"
+	"os"
+	"path/filepath"
+)
+
+func main() {
+	conn, err := net.Dial("unix", filepath.Join(os.TempDir(), "unixDSsample"))
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest("get", "http://localhost:8888", nil)
+	if err != nil {
+		panic(err)
+	}
+
+	req.Write(conn)
+	res, err := http.ReadResponse(bufio.NewReader(conn), req)
+	if err != nil {
+		panic(err)
+	}
+
+	dump, err := httputil.DumpResponse(res, true)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(dump))
+}
